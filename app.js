ab676a7fc658211185449c7634e7d239414ee992
Lucas McDaniel <ldmcdaniel@gmail.com>
2015-08-05 09:35:37 -0500

    changing loggly token

--- app.js
+++ app.js
@@ -1,29 +1,51 @@
+var fs = require('fs');
+
 var express = require('express');
 var lessCSS = require('less-middleware');
+var morgan = require('morgan');
+var loggly = require('loggly');
+var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var pizza = require('./routes/pizza');
+var chickenNuggets = require('./routes/chickennuggets');
 
 var app = express();
 
 app.set('view engine', 'ejs');
 app.set('case sensitive routing', true);
 
+app.locals.title = "aweso.me";
+
 app.use(lessCSS('public'));
 
-app.use(function (req, res, next) {
-  //logging at the top
-  console.log('Request at ' + new Date().toISOString());
-  next();
+// app.use(function (req, res, next) {
+//   //logging at the top
+//   console.log('Request at ' + new Date().toISOString());
+//   next();
+// });
+
+var logStream = fs.createWriteStream('access.log', {flags: 'a'});
+app.use(morgan('combined', {stream: logStream}));
+app.use(morgan('dev'));
+
+var client = loggly.createClient({
+  token: '59511982-7b6a-4995-9119-eb07bcfe17c9',
+  subdomain: 'ldmcdaniel',
+  tags: ['NodeJS'],
+  json: true
 });
+
+client.log("Hello World!");
 
 app.use(express.static('public'));
 
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.use('/', routes);
 app.use('/pizza', pizza);
+app.use('/chickennuggets', chickenNuggets);
 app.set('strict routing', true);
-
-app.locals.title = "aweso.me";
 
 app.use(function (req, res) {
   res.status(403).send('Unauthorized!');
---
+++ routes/chickennuggets.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function (req, res) {
+  res.render('templates/chickennuggets');
+});
+
+router.post('/order', function (req, res) {
+  console.log(req.body);
+  // res.send('Thank you for ordering!');
+  res.redirect('/');
+});
+
+module.exports = router;

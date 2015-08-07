323ee1a49323ec0520b675b43c4b7ae3ea0f4c62
Lucas McDaniel <ldmcdaniel@gmail.com>
2015-08-07 11:24:37 -0500

    working order form and order index

--- routes/chickennuggets.js
+++ routes/chickennuggets.js
@@ -1,13 +1,33 @@
 var express = require('express');
+var moment = require('moment');
+
 var router = express.Router();
 
 router.get('/', function (req, res) {
-  res.render('templates/chickennuggets');
+  var collection = global.db.collection('chickenNuggets');
+
+  collection.find().toArray(function (err, orders) {
+    var formattedOrders = orders.map(function(order) {
+      return {
+        name:       order.name,
+        flavor:     order.style,
+        qty:        order.qty,
+        createdAt:  moment(order._id.getTimestamp()).fromNow()
+      }
+    });
+    res.render('templates/chicken-index', {orders: formattedOrders});
+  });
+});
+
+router.get('/order', function (req, res) {
+  res.render('templates/chicken-new');
 });
 
 router.post('/order', function (req, res) {
-  console.log(req.body);
-  res.redirect('/')
+  var collection = global.db.collection('chickenNuggets');
+  collection.save(req.body, function () {
+    res.redirect('/chickennuggets');
+  });
 });
 
 module.exports = router;
